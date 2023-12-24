17679266e7857d1c8b737b71074df844d43591d0
Sethu Madhavan <62246623+sethu-mad@users.noreply.github.com>
2023-12-24 15:02:53 +0530

    related.js

--- related/related.js
+++ related/related.js
@@ -1,16 +1,18 @@
 import { LightningElement, api, track, wire } from 'lwc';
 import pubsub from 'c/pubsub';
-import {CurrentPageReference} from 'lightning/navigation';
+import {CurrentPageReference} from 'lightning/navigation';//pubsub operator needs a pageRef
 export default class Related extends LightningElement {
-    @track selectedAccountId;
     @api accountId = '';
     @track showContacts = false;
     @wire(CurrentPageReference) pageRef;
     connectedCallback(){
+        //using the below pubsub we're receiving the values passed from searchResult
         pubsub.registerListener('messageevent', this.handleEvent, this);
     }
+    //the below function is called from the above pubsub, you can see this function mentioned in that pubsub
+    //this assigns the accountId passed from searchResult to the accountId variable
     handleEvent(messageFromEvt){
-        this.accountId = messageFromEvt ? messageFromEvt : 'No Message'; 
+        this.accountId = messageFromEvt ? messageFromEvt : 'No Message'; //if no id is passed from searchResult displayes No Message
         console.log(this.accountId);
     }
 }
