d13f51ade84d7f4b4810306871070763e60ea3aa
Sethu Madhavan <62246623+sethu-mad@users.noreply.github.com>
2023-12-24 15:04:58 +0530

    searchComponent.js

--- searchComponent/searchComponent.js
+++ searchComponent/searchComponent.js
@@ -1,23 +1,17 @@
 import { LightningElement, track, api } from 'lwc';
 
 export default class SearchComponent extends LightningElement {
-    //@track showSearchResult = false;
-    @track accountName = '';
-    @track selectedRecordName = '';
-    recordName;
-    //@api selectedAccount = '';
-    @track selectedAccountId;
-    @api AccountId;
+
+    @track accountName = ''; //this stores the input from user and pass it to searchResult
+    @track selectedRecordName = '';//this is the values to gets the accountName from searchResult
+    recordName; //used store the record name from searchResult to display the selected Account Name
+    @track selectedAccountId; //this holds the AccountId
 
     showSearchResult = false;
-    // = false;
-    handleInputChange(event){
-        //alert(event.keyCode);
-        //console.log(event);
+        handleInputChange(event){
         this.accountName = event.target.value;
         if(event.keyCode === 13){
-            this.showSearchResult = true;   
-            //this.showtable = true;
+            this.showSearchResult = true;
             console.log('Enter Key is pressed', this.accountName);
             console.log('value passed to searchResult');
         }
@@ -29,18 +23,8 @@ export default class SearchComponent extends LightningElement {
     handleRecordSelect(event){
         this.recordName = event.detail.selectedRecordName;
         this.selectedAccountId = event.detail.selectedAccount;
-        //this.selectedRecordName = accountName.Name;
-        const selectEvent = new CustomEvent('displaycontact', {
-            detail : {
-                AccountId : this.selectedAccountId
-            }
-        });
-        this.dispatchEvent(selectEvent);
         console.log('passed from child to parent' +this.recordName);
         console.log('passed from search to related component' +this.selectedAccountId);
     }
-    // handleContact(event){
-    //     this.selectedAccountId = event.detail.selectedAccount;
-    //     console.log('Passed ID value: ' +this.selectedAccountId);
-    // }
+
 }
